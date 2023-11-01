44a995042e206c3583784f47264f41cf95d49398
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2023-11-01 13:29:33 +0300

    1st

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,67 @@
-const key = new Key();
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person) {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person has entered the house.');
+      } else {
+        console.log('The door is closed. Person cannot enter the house.');
+      }
+    }
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key) {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is open.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+  }
+  
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
 
-house.openDoor(person.getKey());
+  house.openDoor(person.getKey());
 
-house.comeIn(person);
-
-
-export {};
+  house.comeIn(person);
+  export {};
+  
